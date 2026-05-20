05c4b677b7d75be7d09b32723086faaf2e6bb1bc
Eggsbennedit <marcosssantos1997@gmail.com>
2026-05-20 12:15:50 -0300

    Questões 2021_K, 2022_A, 2022_I, 2022_J, 2023_L feitas

---
+++ MaratonaSBC/2021_K/MARCOS.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T, D, M;
+    cin >> T >> D >> M;
+
+    char d = 'N';
+    int ultimaRef;
+    cin >> ultimaRef;
+    for(int i = 0; i < M-1; i++){
+        int ref;
+        cin >> ref;
+        if(ref - ultimaRef >= T) d = 'Y';
+        ultimaRef = ref;
+    }
+
+    cout << d << '\n';
+
+    return 0;
+}
