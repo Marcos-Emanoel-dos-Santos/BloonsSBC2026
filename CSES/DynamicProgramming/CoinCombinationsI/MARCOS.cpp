b34122014e1f95f6ad393c1bd3f89d6b40c605ad
Eggsbennedit <marcosssantos1997@gmail.com>
2026-07-18 20:07:49 -0300

    CSES CoinCombinationsI resolvido

---
+++ CSES/DynamicProgramming/CoinCombinationsI/MARCOS.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, x; cin >> n >> x;
+    vector<int> coins(n);
+    vector<int> dp(x+1, 0);
+    for(int i = 0; i < n; i++) cin >> coins[i];
+
+    dp[0] = 1;
+
+    for(int i = 1; i <= x; i++){
+        for(int j = 0; j < n; j++){
+            if(i - coins[j] >= 0){
+                dp[i] = (dp[i] + dp[i - coins[j]]) % MOD;
+            }
+        }
+    }
+
+    cout << dp[x] << '\n';
+
+
+    return 0;
+}
