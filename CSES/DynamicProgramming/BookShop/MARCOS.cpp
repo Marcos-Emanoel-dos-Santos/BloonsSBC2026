334ae4f376177bfcbf0e596e3c1914270140d769
Eggsbennedit <marcosssantos1997@gmail.com>
2026-07-19 00:40:53 -0300

    CSES GridPathsI e BookShop resolvidos

---
+++ CSES/DynamicProgramming/BookShop/MARCOS.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, x; cin >> n >> x;
+    vector<int> prices(n);
+    for(int i = 0; i < n; i++) cin >> prices[i];
+    vector<int> pages(n);
+    for(int i = 0; i < n; i++) cin >> pages[i];
+
+    vector<int> dp(x + 1, 0);
+
+    for(int i = 0; i < n; i++){
+        for(int j = x; j >= prices[i]; j--){
+            dp[j] = max(dp[j], dp[j - prices[i]] + pages[i]);
+        }
+    }
+
+    cout << dp[x] << '\n';
+
+    return 0;
+}
